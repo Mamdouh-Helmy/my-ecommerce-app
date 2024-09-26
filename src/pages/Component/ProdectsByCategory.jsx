0044b6c0dd8d90380202fd993d6a946eadc3cf3d
Mamdouh-Helmy <mm9349263@gmail.com>
2024-09-26 04:30:50 +0300

    add: changes

--- src/pages/Component/ProdectsByCategory.jsx
+++ src/pages/Component/ProdectsByCategory.jsx
@@ -2,13 +2,15 @@ import { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../../context/ProductsProvider ";
 import { ShoppingCart, Eye, Heart } from "../../utils/icons.util";
 import StarRating from "./StarRating";
+import ProductPopup from "./ProductPopup";
 
 export default function ProdectsByCategory({ category }) {
-  const { products, addToCart } = useContext(ProductsContext);
+  const { products, addToCart, cart } = useContext(ProductsContext);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [hoveredIcons, setHoveredIcons] = useState({});
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   const DataUrl = category?.url;
   useEffect(() => {
@@ -51,65 +53,92 @@ export default function ProdectsByCategory({ category }) {
     setHoveredIcons({ ...hoveredIcons, [icon]: false });
   };
 
+  const openPopup = (product) => {
+    setSelectedProduct(product);
+  };
+
+  const closePopup = () => {
+    setSelectedProduct(null);
+  };
+
   return (
     <div className="box">
       <div className="prodect-box">
         {data.length > 0 ? (
-          data.map((product, index) => (
-            <div key={index} className="product">
-              <img src={product.thumbnail} alt={product.title} />
-              <div className="text">
-                <div className="count">
-                  <StarRating rating={product.rating} />
-                  <span className="count-prodect">({product.stock})</span>
-                </div>
+          data.map((product, index) => {
+            const isProductInCart = cart.some(
+              (cartItem) => cartItem.id === product.id
+            );
 
-                <h3>{product.title}</h3>
-                <p>${product.price}</p>
-                <div className="button">
-                  <button
-                    onMouseEnter={() => handleMouseEnter("heart-" + index)}
-                    onMouseLeave={() => handleMouseLeave("heart-" + index)}
-                  >
-                    <Heart
-                      className="icon"
-                      color={
-                        hoveredIcons["heart-" + index] ? "#FFFFFF" : "#191C1F"
-                      }
-                      size="20"
-                    />
-                  </button>
-                  <button
-                    onMouseEnter={() => handleMouseEnter("cart-" + index)}
-                    onMouseLeave={() => handleMouseLeave("cart-" + index)}
-                    onClick={() => addToCart(product)}
-                  >
-                    <ShoppingCart
-                      color={
-                        hoveredIcons["cart-" + index] ? "#FFFFFF" : "#191C1F"
-                      }
-                      size="20"
-                    />
-                  </button>
-                  <button
-                    onMouseEnter={() => handleMouseEnter("eye-" + index)}
-                    onMouseLeave={() => handleMouseLeave("eye-" + index)}
-                  >
-                    <Eye
-                      size="20"
-                      color={
-                        hoveredIcons["eye-" + index] ? "#FFFFFF" : "#191C1F"
-                      }
-                    />
-                  </button>
+            return (
+              <div key={index} className="product">
+                <img src={product.thumbnail} alt={product.title} />
+                <div className="text">
+                  <div className="count">
+                    <StarRating rating={product.rating} />
+                    <span className="count-prodect">({product.stock})</span>
+                  </div>
+
+                  <h3>{product.title}</h3>
+                  <p>${product.price}</p>
+                  <div className="button">
+                    <button
+                      onMouseEnter={() => handleMouseEnter("heart-" + index)}
+                      onMouseLeave={() => handleMouseLeave("heart-" + index)}
+                    >
+                      <Heart
+                        className="icon"
+                        color={
+                          hoveredIcons["heart-" + index] ? "#FFFFFF" : "#191C1F"
+                        }
+                        size="20"
+                      />
+                    </button>
+                    <button
+                      onMouseEnter={() => handleMouseEnter("cart-" + index)}
+                      onMouseLeave={() => handleMouseLeave("cart-" + index)}
+                      onClick={() => addToCart(product)}
+                      style={{
+                        cursor: `${isProductInCart ? "no-drop" : "pointer"}`,
+                        pointerEvents: `${isProductInCart ? "none" : "all"}`,
+                        opacity: `${isProductInCart ? "0.5" : "1"}`,
+                      }}
+                      disabled={isProductInCart}
+                    >
+                      <ShoppingCart
+                        color={
+                          hoveredIcons["cart-" + index] ? "#FFFFFF" : "#191C1F"
+                        }
+                        size="20"
+                      />
+                    </button>
+                    <button
+                      onMouseEnter={() => handleMouseEnter("eye-" + index)}
+                      onMouseLeave={() => handleMouseLeave("eye-" + index)}
+                      onClick={() => openPopup(product)}
+                    >
+                      <div className="deatiles-prodects">
+                        <Eye
+                          size="20"
+                          color={
+                            hoveredIcons["eye-" + index] ? "#FFFFFF" : "#191C1F"
+                          }
+                        />
+                      </div>
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))
+            );
+          })
         ) : (
           <div>No products available</div>
         )}
       </div>
+
+      {selectedProduct && (
+        <ProductPopup product={selectedProduct} onClose={closePopup} />
+      )}
     </div>
   );
 }
---
+++ src/pages/Component/ProductPopup.jsx
@@ -0,0 +1,85 @@
+import StarRating from "./StarRating";
+import { ShoppingCart } from "../../utils/icons.util";
+import { useContext } from "react";
+import { ProductsContext } from "../../context/ProductsProvider ";
+
+const ProductPopup = ({ product, onClose }) => {
+  const { cart, addToCart } = useContext(ProductsContext);
+
+  const isProductInCart = cart.some((cartItem) => cartItem.id === product.id);
+
+  const handleAddToCart = () => {
+    if (!isProductInCart) {
+      addToCart(product);
+    }
+  };
+//   console.log(product);
+  return (
+    <div className="pop">
+      <div className="box-pop">
+        <button onClick={onClose}>x</button>
+        <div className="content">
+          <div className="box">
+            <div className="images">
+              <div className="imge">
+                <img src={product.thumbnail} alt={product.title} />
+              </div>
+            </div>
+          </div>
+          <div className="box">
+            <div className="rating-star">
+              <StarRating rating={product.rating} />
+              <div className="rating">{product.rating} Star Rating</div>
+            </div>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <div className="dateiles-prodects-pop">
+              <div className="text">
+                <div className="fact">
+                  <span>Sku: </span> {product.sku}
+                </div>
+                <div className="fact">
+                  <span>Brand: </span>
+                  {product.brand || "No specific brand"}
+                </div>
+              </div>
+              <div className="text">
+                <div
+                  className="fact"
+                  style={{
+                    color: `${
+                      product.availabilityStatus == "In Stock"
+                        ? "#2db224"
+                        : "#ee5858"
+                    }`,
+                  }}
+                >
+                  <span>Availability: </span> {product.availabilityStatus}
+                </div>
+                <div className="fact">
+                  <span>Category :</span> {product.category}
+                </div>
+              </div>
+            </div>
+            <span className="price">${product.price}</span>
+            <button
+              className="add-cart"
+              onClick={handleAddToCart}
+              disabled={isProductInCart}
+              style={{
+                cursor: `${isProductInCart ? "no-drop" : "pointer"}`,
+                pointerEvents: `${isProductInCart ? "none" : "all"}`,
+                opacity: `${isProductInCart ? "0.5" : "1"}`,
+              }}
+            >
+              {isProductInCart ? "Not add to cart" : "Add to cart"}
+              <ShoppingCart size="20" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductPopup;
